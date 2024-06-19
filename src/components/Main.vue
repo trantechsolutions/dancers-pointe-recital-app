4838c023576acc1c0910ced5a65394a1e78bd9ba
Jonathan Tran <me@jonathanvutran.com>
2024-06-19 18:08:40 -0500

    created expandable to make more legible on mobile device

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -14,18 +14,24 @@
               ></v-text-field>
             </template>
         
-            <v-data-table :headers="headers" :items="filteredData" :search="search" :items-per-page="0"
-              hide-default-footer disable-sort>
+            <v-data-table :headers="headers" :items="filteredData" :search="search" :items-per-page="0" :item-value="getGuid"
+              hide-default-footer disable-sort show-expand>
 
               <template v-slot:header.performance="{ column }">
                 <v-select :items="dropdown" v-model="performance" label="Performance">
                 </v-select>
               </template>
 
-              <template v-slot:item.participants="{ item }">
-                <div class="text-end">
-                  <v-chip class="ma-1" size="default" v-for="(performer, index) in item.participants">{{ performer }}</v-chip>
-                </div>
+              <template v-slot:expanded-row="{ columns, item }">
+                <tr>
+                  <td :colspan="columns.length">
+                    <strong>Participants</strong>
+                    <p v-if="item.participants.length <= 0"><v-chip class="ma-1" size="default" >{{ item.performance_name }}</v-chip></p>
+                    <p>
+                      <v-chip class="ma-1" size="default" v-for="(performer, index) in item.participants">{{ performer }}</v-chip>
+                    </p>
+                  </td>
+                </tr>
               </template>
             </v-data-table>
           </v-card>
@@ -37,7 +43,9 @@
 
   export default {
     methods: {
-
+        getGuid(item) {
+          return `${item.performance}${item.performance_number}`
+        }
       },
       computed: {
         title() {
@@ -55,7 +63,7 @@
       },
       data: () => ({
         search: '',
-        headers: [{ title: 'Performance', key: 'performance', width: '20%' }, { title: 'Number', key: 'performance_number' }, { title: 'Title', key: 'performance_name' }, { title: 'Participants', key: 'participants' }],
+        headers: [{ title: 'Performance', key: 'performance', width: '20%' }, { title: 'Number', key: 'performance_number' }, { title: 'Title', key: 'performance_name' }],
         jsonData: [],
         dropdown: [
           { title: 'All' },
