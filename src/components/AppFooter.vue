598b84dea68cb8e793d4d65d8a066f9d6d5b3740
Jonathan Tran <me@jonathanvutran.com>
2024-06-20 10:35:39 -0500

    updated footer information

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -11,7 +11,7 @@
     >
       <v-icon
         :icon="item.icon"
-        :size="item.icon === '$vuetify' ? 24 : 16"
+        :size="16"
       />
     </a>
 
@@ -19,16 +19,7 @@
       class="text-caption text-disabled"
       style="position: absolute; right: 16px;"
     >
-      &copy; 2016-{{ (new Date()).getFullYear() }} <span class="d-none d-sm-inline-block">Vuetify, LLC</span>
-      —
-      <a
-        class="text-decoration-none on-surface"
-        href="https://vuetifyjs.com/about/licensing/"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        MIT License
-      </a>
+      &copy; {{ (new Date()).getFullYear() }} <span class="d-none d-sm-inline-block">Dancer's Pointe, LLC</span>
     </div>
   </v-footer>
 </template>
@@ -36,34 +27,14 @@
 <script setup>
   const items = [
     {
-      title: 'Vuetify Documentation',
-      icon: `$vuetify`,
-      href: 'https://vuetifyjs.com/',
+      title: 'Dancer\'s Pointe Website',
+      icon: 'mdi-earth',
+      href: 'https://dancers-pointe.com/',
     },
     {
-      title: 'Vuetify Support',
-      icon: 'mdi-shield-star-outline',
-      href: 'https://support.vuetifyjs.com/',
-    },
-    {
-      title: 'Vuetify X',
-      icon: `svg:M2.04875 3.00002L9.77052 13.3248L1.99998 21.7192H3.74882L10.5519 14.3697L16.0486 21.7192H22L13.8437 10.8137L21.0765 3.00002H19.3277L13.0624 9.76874L8.0001 3.00002H2.04875ZM4.62054 4.28821H7.35461L19.4278 20.4308H16.6937L4.62054 4.28821Z`,
-      href: 'https://x.com/vuetifyjs',
-    },
-    {
-      title: 'Vuetify GitHub',
-      icon: `mdi-github`,
-      href: 'https://github.com/vuetifyjs/vuetify',
-    },
-    {
-      title: 'Vuetify Discord',
-      icon: `mdi-discord`,
-      href: 'https://community.vuetifyjs.com/',
-    },
-    {
-      title: 'Vuetify Reddit',
-      icon: `mdi-reddit`,
-      href: 'https://reddit.com/r/vuetifyjs',
+      title: 'Facebook - Dancer\'s Pointe',
+      icon: 'mdi-facebook',
+      href: 'https://www.facebook.com/DancersPointeDP/',
     },
   ]
 </script>
